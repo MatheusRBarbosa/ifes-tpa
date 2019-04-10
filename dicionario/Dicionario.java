1d2e74b6574cb44f2209fe2606113c07440d51b9
Matheus Barbosa <matheuskleber@live.com>
2019-04-09 21:37:18 -0300

    Redimensionamento burro

--- dicionario/Dicionario.java
+++ dicionario/Dicionario.java
@@ -11,62 +11,89 @@ import java.util.LinkedList;
  *
  * @author barbosa
  */
-public class Dicionario<K,V> {
+public class Dicionario<K, V> {
+
     private LinkedList<Conteudo>[] dicionario;
     private HashEngine hashEngine;
-    private int len = 0;
-    
-    public Dicionario(){
+    private int len = 0; //Quantidade de elementos
+
+    public Dicionario() {
         this.hashEngine = new DefaultHashEngine();
         int tam = 200;
         this.dicionario = new LinkedList[tam];
-        for(int i=0;i<tam;i++){
+        for (int i = 0; i < tam; i++) {
             this.dicionario[i] = new LinkedList<>();
         }
     }
-    
-    public Dicionario(int tam){
+
+    public Dicionario(int tam) {
         this.hashEngine = new DefaultHashEngine();
         double fc = 0.75;
-        int len = (int)(tam/fc);
+        int len = (int) (tam / fc);
         this.dicionario = new LinkedList[len];
-        for(int i=0;i<len;i++){
+        for (int i = 0; i < len; i++) {
             this.dicionario[i] = new LinkedList<>();
         }
     }
-    
-    public Dicionario(int tam, HashEngine hashEngine){
+
+    public Dicionario(int tam, HashEngine hashEngine) {
         this.hashEngine = hashEngine;
         double fc = 0.75;
-        int len = (int)(tam/fc);
+        int len = (int) (tam / fc);
         this.dicionario = new LinkedList[len];
-        for(int i=0;i<len;i++){
+        for (int i = 0; i < len; i++) {
             this.dicionario[i] = new LinkedList<>();
         }
     }
-    
-    public int len(){
+
+    public int len() {
         return this.len;
     }
-    
-    public int getIndex(K key){
+
+    public int getIndex(K key) {
         Long hash = this.hashEngine.generateHash(key);
-        return (int)(hash % this.dicionario.length);
+        return (int) (hash % this.dicionario.length);
     }
-    
-    public void insert(K key, V item){
-       int index = getIndex(key);
-       this.dicionario[index].add(new Conteudo(key, item));
-       this.len++;
+
+    private int getMaiorLista() {
+        int tam = this.dicionario[0].size();
+        for (int i = 1; i < this.dicionario.length; i++) {
+            if (tam < this.dicionario[i].size()) {
+                tam = this.dicionario[i].size();
+            }
+        }
+        return tam;
     }
-    
-    private Conteudo<K,V> findConteudo(K key){
+
+    private void redimensiona() {
+        
+        int newTam = this.dicionario.length * 2;
+        LinkedList<Conteudo>[] newDicionario = new LinkedList[newTam];
+        for (int i = 0; i < this.dicionario.length; i++) {
+            newDicionario[i] = this.dicionario[i];
+        }
+        for(int i = this.dicionario.length; i<newTam; i++){
+            newDicionario[i] = new LinkedList<>();
+        }
+        this.dicionario = newDicionario;
+    }
+
+    public void insert(K key, V item) {
+        int index = getIndex(key);
+        this.dicionario[index].add(new Conteudo(key, item));
+        this.len++;
+        if (this.getMaiorLista() >= (int) (this.dicionario.length * 0.5)) {
+            this.redimensiona();
+        }
+    }
+
+    private Conteudo<K, V> findConteudo(K key) {
         int index = this.getIndex(key);
-        if(!this.dicionario[index].isEmpty()){
-            int i=0;
-            while(i < this.dicionario[index].size()){
+        if (!this.dicionario[index].isEmpty()) {
+            int i = 0;
+            while (i < this.dicionario[index].size()) {
                 Conteudo c = this.dicionario[index].get(i);
-                if(c != null && key.equals(c.getKey())){
+                if (c != null && key.equals(c.getKey())) {
                     return c;
                 }
                 i++;
@@ -74,26 +101,26 @@ public class Dicionario<K,V> {
         }
         return null;
     }
-    
-    public V findByKey(K key){
+
+    public V findByKey(K key) {
         return this.findConteudo(key).getConteudo();
     }
-    
-    public void remove(K key){
+
+    public void remove(K key) {
         int index = this.getIndex(key);
         System.out.println(index);
-        if(!this.dicionario[index].isEmpty()){
+        if (!this.dicionario[index].isEmpty()) {
             this.dicionario[index].remove();
             this.len--;
         }
     }
-    
-    public int[] getColisoes(){
+
+    public int[] getColisoes() {
         int[] colisoes = new int[this.dicionario.length];
-        for(int i=0; i<this.dicionario.length;i++){
+        for (int i = 0; i < this.dicionario.length; i++) {
             colisoes[i] = this.dicionario[i].size();
         }
         return colisoes;
     }
-    
+
 }
--- dicionario/Main.java
+++ dicionario/Main.java
@@ -8,15 +8,15 @@ package exerciciostpa.dicionario;
 import java.util.Arrays;
 
 class Pessoa {
-    private String nome;
+    private int nome;
     private int idade;
     
-    public Pessoa(String nome, int idade){
+    public Pessoa(int nome, int idade){
         this.nome = nome;
         this.idade = idade;
     }
     
-    public String getNome(){
+    public int getNome(){
         return this.nome;
     }
     
@@ -32,24 +32,14 @@ class Pessoa {
 public class Main {
     
     public static void main(String[] args) {
-        Dicionario<Integer, Pessoa> dic = new Dicionario<>(12);
+        Dicionario<Integer, Pessoa> dic = new Dicionario<>(4);
         System.out.println("Hello");
         
-        dic.insert(1231, new Pessoa("1", 123123));
-        dic.insert(123123, new Pessoa("Dois", 222));
-        dic.insert(55555, new Pessoa("tres", 55));
-        dic.insert(777, new Pessoa("quatro", 4));
-        dic.insert(9999, new Pessoa("cinco", 5));
-        dic.insert(1010, new Pessoa("seis", 6));
-        dic.insert(1111, new Pessoa("sete", 7));
-        dic.insert(5, new Pessoa("oito", 8));
-        dic.insert(92928, new Pessoa("nove", 9));
-        dic.insert(12341, new Pessoa("dez",10));
-        dic.insert(1092832109, new Pessoa("onze",11));
-        dic.insert(10923, new Pessoa("doze",12));
+        for(int i=1;i<100;i++){
+            dic.insert(i, new Pessoa(i,i));
+        }
+        //System.out.println(dic.findByKey(5).getNome());
         
-        System.out.println(dic.findByKey(5).getNome());
-        
-        System.out.println(Arrays.toString(dic.getColisoes()));   
+        System.out.println(Arrays.toString(dic.getColisoes()));
     }
 }
