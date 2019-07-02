991cfca82defdc1e1530a30a2a8793a78a38ffa7
Matheus Barbosa <matheuskleber@live.com>
2019-07-02 19:24:20 -0300

    Criando classes vazias

---
+++ ifes/bsi/tpa/Tree/Aplicacao/Board.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ifes.bsi.tpa.Tree.Aplicacao;
+
+/**
+ *
+ * @author User
+ */
+public class Board {
+    //Classe que usa a arvore para gerar o tabuleiro
+    //O tabuleiro de fato eh basicamente uma matriz
+}
